74222b031e43d60bf11481178f501255b6b082f5
tashtanovnurjigit <wwef432@gmail.com>
2023-05-25 01:50:07 +0600

    lesson3

--- movie/views.py
+++ movie/views.py
@@ -6,20 +6,44 @@ from . import serializers
 from .models import Movie
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list_api_view(request):
-    movies = Movie.objects.all()
-    serializer = serializers.MovieListSerializers(movies, many=True)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = serializers.MovieListSerializers(movies, many=True)
 
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        data = request.data
+
+        movie = Movie.objects.create(
+            director_id=data.get('director_id'),
+            title=data.get('title'),
+            description=data.get('description'),
+            rate=data.get('rate')
+        )
+        movie.genres.set(data.get('genres'))
+        return Response(data=serializers.MovieListSerializers(movie, many=False).data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def movie_detail_api_view(request, **kwargs):
     try:
         movie = Movie.objects.get(id=kwargs['id'])
     except Movie.DoesNotExist:
         return Response({'error': 'Movie not found!'}, status=status.HTTP_404_NOT_FOUND)
-    serializer = serializers.MovieDetailSerializer(movie, many=False)
+    if request.method == 'GET':
+        serializer = serializers.MovieDetailSerializer(movie, many=False)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        data = request.data
 
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
+        movie.director_id = data.get('director_id')
+        movie.title = data.get('title')
+        movie.description = data.get('description')
+        movie.rate = data.get('rate')
+        movie.genres.set(data.get('genres'))
+
+        movie.save()
+        return Response(data=serializers.MovieDetailSerializer(movie, many=False).data, status=status.HTTP_200_OK)
